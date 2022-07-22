58ac32940131abc84e81e7deeb86809a0d8633b0
Adrian Pothuaud <adrian@trusk.com>
2022-07-22 16:32:49 +0200

    project setup eslint, typescript, jest

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    'jest/globals': true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'jest',
+  ],
+  rules: {
+    'max-len': ['error', 150],
+  },
+};
